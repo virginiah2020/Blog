06f3109794d39381977d0141add57bfc8094eefe
virginiah2020 <vancyvy254@gmail.com>
2020-07-20 22:55:43 +0300

    def load user

--- models.py
+++ models.py
@@ -1,5 +1,10 @@
 from datetime import datetime
 from flaskblog import db,login manager
+from flask_login import UserMixin
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
