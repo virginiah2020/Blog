3d8bd702ed338b9cca3b00f68a9846639a0d5f51
virginiah2020 <vancyvy254@gmail.com>
2020-07-20 18:34:42 +0300

    installed flask-sqlalchemy

--- flaskblog.py
+++ flaskblog.py
@@ -1,14 +1,29 @@
 from flask import Flask , render_template, url_for, flash, redirect
+from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LoginForm
 
 
 # "Import flask and render template ,
 app = Flask(__name__)# app is a flask variable whihc is set to an instance ,Add flask variable,
-
-
 app.config['SECRET_KEY'] = 'f4d64e66de7a5102a52fd22dcd9e08a0'
+app.config['sQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
 
-# 
+class User(db.model):
+     id = db.Column(db.Interger, primary_key=True)
+     username = db.Column(db.String(20), unique=True, nullable=False)
+     username = db.Column(db.String(120), unique=True, nullable=False)
+     image_file = db.Column(db.Sring(20), nullable=False, default='default.jpg' )
+     password = db.Column(db.Sring(60), nullable=False)
+
+     def __repr__(self):
+         return f"User('{self.username}','{self.email}','{self.image_file}')"
+
+class Post(db.model):
+     id = db.Column(db.Interger, primary_key=True)
+     title = db.Column(db.String(100), nullable=False)
+     date_posted = db.Column(db.DateTime, nullable=False, )
+
 #  Add dictionary which represents a single blog post"
 posts = [
     {
