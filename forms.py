d00b553fd71d8d535c8f57da789bdb3a3735f2d0
virginiah2020 <vancyvy254@gmail.com>
2020-07-20 23:11:02 +0300

    function to validate email

--- forms.py
+++ forms.py
@@ -18,12 +18,16 @@ class RegistrationForm(FlaskForm):
 
     submit = SubmitField('Sign Up')
 
-     def validate_username(self, username):
+    def validate_username(self, username):
         user = User.query.filter_by(username=username.data).first()
         if user:
             raise ValidationError('That username is taken. Please choose a different one.')
-
-
+    
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('That email is taken. Please choose a different one.')
+    
 
 
 class LoginForm(FlaskForm):
