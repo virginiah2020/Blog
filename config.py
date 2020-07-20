0ac4621db45e31577b792bda347de1fa12fc3ebe
virginiah2020 <vancyvy254@gmail.com>
2020-07-20 23:35:25 +0300

    os environ for email

--- config.py
+++ config.py
@@ -5,3 +5,6 @@ class Config:
     SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI')
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get('EMAIL_USER')
+    MAIL_PASSWORD = os.environ.get('EMAIL_PASS')
