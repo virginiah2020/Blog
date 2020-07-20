9dba4121a782c6e96eaa748f65085e7008cdd06c
virginiah2020 <vancyvy254@gmail.com>
2020-07-20 22:51:34 +0300

    installed flask-login

--- __init__.py
+++ __init__.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'f4d64e66de7a5102a52fd22dcd9e08a0'
