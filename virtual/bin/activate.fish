5ada3b8a3010c80db3711abad63da0efd742c638
virginiah2020 <vancyvy254@gmail.com>
2020-07-21 21:12:32 +0300

    installed migration

--- virtual/bin/activate.fish
+++ virtual/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/moringa-school-1063/Downloads/pitch-post/virtual"
+set -gx VIRTUAL_ENV "/home/moringa-school-1063/Desktop/Moringa-school-projects/vy-blog/virtual"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
